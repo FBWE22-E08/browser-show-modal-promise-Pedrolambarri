eb955e5487d2b8686df2780f5be1b60318765109
Pedro Lambarri <plambarri@gmail.com>
2023-01-30 14:20:11 +0100

    promises-javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./css/style.css"; //importing css
+
+
+const modal = document.querySelector(".modal");
+const span = document.querySelector(".close");
+
+const messagePromise = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(modal.style.display = "block");
+      }, 6000);
+    });
+  };
+
+const solving = async() => {
+    let result = await messagePromise();
+    return result
+}
+
+  solving();
+  
+  span.style.cursor = "pointer";
+
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+  })
+
